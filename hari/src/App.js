5d7f7e739f05e3ad4306a59945fc46796285dafd
venkiarms2391 <venkiarms2391@gmail.com>
2023-04-28 11:34:26 +0530

    My Project

---
+++ hari/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, {useState,useEffect} from 'react';
+
+function App() {
+  const [first, setfirst] = useState('livewire')
+  
+  const inc = () =>{
+    setfirst('users')
+  }
+
+  useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/${first}`)
+      .then(response => response.json())
+      .then(json => console.log(json))
+
+  },[first])
+return (
+    <div className="App">
+      <button onClick={inc}>Click</button>
+      {first}
+    </div>
+  );
+}
+
+export default App;
